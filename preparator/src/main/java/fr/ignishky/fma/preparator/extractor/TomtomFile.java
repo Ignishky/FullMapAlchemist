62257d814ee3244922a0f7885e62c702886cbd0f
ignishky <thierry.ducloyer@gmail.com>
2019-04-13 18:28:54 +0200

    [FMA-10] As an alchemist, I want to download railroads shapefiles.

--- preparator/src/main/java/fr/ignishky/fma/preparator/extractor/TomtomFile.java
+++ preparator/src/main/java/fr/ignishky/fma/preparator/extractor/TomtomFile.java
@@ -1,18 +1,15 @@
 package fr.ignishky.fma.preparator.extractor;
 
-import lombok.Getter;
-
 import java.util.stream.Stream;
 
 public enum TomtomFile {
 
     BOUNDARIES_LEVEL0("mn", "______________a0."),
     ALTERNATE_NAMES("mn", "_an."),
+    RAILROADS("mn", "_rr."),
     CENTERS_OF_SETTLEMENT("mn", "_sm.");
 
-    @Getter
     private final String product;
-    @Getter
     private final String value;
 
     TomtomFile(String product, String value) {
@@ -23,6 +20,6 @@ public enum TomtomFile {
     public static Stream<String> allFilesFrom(String product) {
         return Stream.of(TomtomFile.values())
                 .filter(tomtomFile -> tomtomFile.product.equals(product))
-                .map(TomtomFile::getValue);
+                .map(tomtomFile -> tomtomFile.value);
     }
 }
